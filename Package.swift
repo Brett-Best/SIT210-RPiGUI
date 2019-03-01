a8b12a54ff1e17eb24f906d8ac8524c7ade4007d
Brett Best <brettbest@live.com.au>
2019-03-02 08:39:46 +1100

    Added package file.

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+  name: "RPiGUI",
+  dependencies: [
+    .package(url: "https://github.com/uraimo/SwiftyGPIO.git", .branch("master")),
+    .package(url: "https://github.com/rhx/SwiftGtk.git", .branch("master")),
+  ],
+  targets: [
+    .target(name: "RPiGUI", dependencies: ["Gtk"]),
+  ]
+)
