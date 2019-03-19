6d65692a8700a68309830c66b80792f828943125
Brett Best <brettbest@live.com.au>
2019-03-20 04:22:32 +1100

    Update package file to use mirror of SwiftGTK.

--- Package.swift
+++ Package.swift
@@ -2,13 +2,30 @@
 
 import PackageDescription
 
+#if os(macOS)
+
 let package = Package(
   name: "RPiGUI",
   dependencies: [
     .package(url: "https://github.com/uraimo/SwiftyGPIO.git", .branch("master")),
-    .package(url: "https://github.com/rhx/SwiftGtk.git", .branch("master")),
+    .package(url: "https://github.com/Brett-Best/SwiftGtk.git", .branch("master"))
   ],
   targets: [
-  .target(name: "RPiGUI", dependencies: ["SwiftyGPIO", "Gtk"]),
+    .target(name: "RPiGUI", dependencies: ["SwiftyGPIO", "Gtk"]),
   ]
 )
+
+#else
+
+let package = Package(
+  name: "RPiGUI",
+  dependencies: [
+    .package(url: "https://github.com/uraimo/SwiftyGPIO.git", .branch("master")),
+    .package(url: "https://github.com/Brett-Best/SwiftGtk.git", .branch("bb-pkg-dev"))
+  ],
+  targets: [
+    .target(name: "RPiGUI", dependencies: ["SwiftyGPIO", "Gtk"]),
+  ]
+)
+
+#endif
